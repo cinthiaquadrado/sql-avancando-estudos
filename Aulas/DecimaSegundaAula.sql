8e76e55f3b1a3060dad6b1bd1c5ad799c210423c
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-09-10 17:45:53 -0300

    Union e subconsultas

---
+++ Aulas/DecimaSegundaAula.sql
@@ -0,0 +1,46 @@
+--Union faz a união de duas ou mais tabelas
+
+--É importante que as tabelas que serão unidas tenham o mesmo número e tipo de campo
+
+--Union all não aplica o distinct sobre o resultado final da consulta
+
+use sucos_vendas
+SELECT DISTINCT BAIRRO FROM tabela_de_clientes
+SELECT DISTINCT BAIRRO FROM tabela_de_vendedores;
+
+--União entre duas tabelas
+SELECT DISTINCT BAIRRO FROM tabela_de_clientes
+UNION
+SELECT DISTINCT BAIRRO FROM tabela_de_vendedores;
+
+--Não aplica o distinct sobre o resultado final da consulta
+SELECT DISTINCT BAIRRO FROM tabela_de_clientes
+UNION ALL
+SELECT DISTINCT BAIRRO FROM tabela_de_vendedores;
+
+SELECT DISTINCT BAIRRO, NOME, 'CLIENTE' as TIPO FROM tabela_de_clientes
+UNION
+SELECT DISTINCT BAIRRO, NOME, 'VENDEDOR' as TIPO FROM tabela_de_vendedores;
+
+--Fica como tipo_cliente porque ele puxa este primeiro
+SELECT DISTINCT BAIRRO, NOME, 'CLIENTE' as TIPO_CLIENTE FROM tabela_de_clientes
+UNION
+SELECT DISTINCT BAIRRO, NOME, 'VENDEDOR' as TIPO_VENDEDOR FROM tabela_de_vendedores;
+
+--Junta CPF e matrícula na mesma coluna
+SELECT DISTINCT BAIRRO, NOME, 'CLIENTE' as TIPO_CLIENTE, CPF  FROM tabela_de_clientes
+UNION
+SELECT DISTINCT BAIRRO, NOME, 'VENDEDOR' as TIPO_VENDEDOR, MATRICULA FROM tabela_de_vendedores;
+
+--Junta todo mundo tendo ou não os respectivos clientes e vendedores
+SELECT tabela_de_vendedores.BAIRRO,
+tabela_de_vendedores.NOME, DE_FERIAS,
+tabela_de_clientes.BAIRRO,
+tabela_de_clientes.NOME  FROM tabela_de_vendedores LEFT JOIN tabela_de_clientes
+ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO
+UNION
+SELECT tabela_de_vendedores.BAIRRO,
+tabela_de_vendedores.NOME, DE_FERIAS,
+tabela_de_clientes.BAIRRO,
+tabela_de_clientes.NOME  FROM tabela_de_vendedores RIGHT JOIN tabela_de_clientes
+ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
